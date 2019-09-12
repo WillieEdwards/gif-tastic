737ee763894114d35d759d2d7e9a559822735648
Willie Edwards <gammaray360@gmail.com>
2019-09-11 19:44:27 -0700

    added button creation and ajax functions in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,3 +5,51 @@ $(document).ready(function () {
         $("#cartoon-buttons").append("<button type='button' onclick='searchGif(\"" + cartoonArray[i] + "\")' class='btn btn-primary' value=' " + cartoonArray[i] + "'> " + cartoonArray[i] + " </button>");
     }
 })
+
+function cartoonButtonClicked() {
+    var userInput = $('#cartoon-input').val();
+    searchGif(userInput);
+}
+
+function submitButtonClicked() {
+    var userInput = $('#cartoon-input').val();
+
+    if (userInput) {
+        $('#cartoon-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
+    }
+}
+
+function searchGif(gifName) {
+    $.ajax({
+            url: 'https://api.giphy.com/v1/gifs/search?q= ' + gifName + ' &api_key=hLbcRnnatQnWOdt4ECo58njKzegfSSep&limit=10"',
+            type: 'GET',
+        })
+        .done(function(response) {
+            displayGif(response);
+        })
+}
+
+function displayGif(response) {
+    $('#cartoon').empty();
+    for (var i = 0; i < response.data.length; i++) {
+        var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
+        var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
+            '" data-still=" ' + response.data[i].images.fixed_height_still.url +
+            ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "width:250px; height:250px">';
+
+        image = '<div class="col-md-4">' + image + "</div>";
+        $('#cartoon').append(image);
+    }
+
+    $('.movImage').on('click', function() {
+        var state = $(this).attr('data-state');
+        if (state == 'still') {
+            $(this).attr('src', $(this).attr("data-animate"));
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).attr("data-still"));
+            $(this).attr('data-state', 'still');
+        }
+
+    });
+}
